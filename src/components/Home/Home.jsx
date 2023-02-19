4208a69eb490125d86696f7dfc33027fa6f51186
cereceda1991 <cereceda1991@gmail.com>
2023-02-19 17:26:24 -0500

    modify2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { changeName } from '../../store/slices/username.slice'
 import { RiSendPlaneFill } from "react-icons/ri";
+import portada from "../../assets/portada.png";
 import './Home.css'
 
 const Home = () => {
@@ -21,7 +22,7 @@ const Home = () => {
         <div className='home__card'>
             <div className='home__card-container'>
                 <h1>Hello Trainer!</h1>
-                <img className='home__card-img' src="/src/assets/portada.png" />
+                <img className='home__card-img' src={portada} />
             </div>
             <h2>Give me you name to start!</h2>
             <form onSubmit={(e) => handleSubmit(e)}>
